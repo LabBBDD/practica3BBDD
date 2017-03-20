1e21063ac7c3e0510f4b669676e22290c9e834ec
Jaime <Jaime.FernandezBravo1@alu.uclm.es>
2017-03-20 11:58:05 +0100

    formulario inicio programa

--- practica3BBDD/Form1.vb
+++ practica3BBDD/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
 
+    Dim ruta As String
+
+    Private Sub LoadForm(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If MessageBox.Show("¿Quieres abrir una base de datos?", "nueva sesion", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Dim ventanaAbrirBD As New OpenFileDialog()
+            ventanaAbrirBD.Title = "Selecciona una base de datos"
+            ventanaAbrirBD.Filter = "Archivos de base de datos|*.accdb"
+
+            If ventanaAbrirBD.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                ventanaAbrirBD.OpenFile()
+                ruta = ventanaAbrirBD.FileName
+            End If
+        End If
+
+
+    End Sub
 End Class
